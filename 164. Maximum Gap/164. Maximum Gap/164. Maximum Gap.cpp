1b27096cd71bf81021684095a9cc9415d53b5ec4
Chen <choak@qq.com>
2018-12-11 22:16:47 +0800

    test

--- 164. Maximum Gap/164. Maximum Gap/164. Maximum Gap.cpp
+++ 164. Maximum Gap/164. Maximum Gap/164. Maximum Gap.cpp
@@ -2,11 +2,45 @@
 //
 
 #include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <set>
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello Git!    \n"; 
+	freopen("in.txt", "r", stdin);
+	int temp;
+	vector<int> nums;
+	while (cin >> temp)
+		nums.push_back(temp);
+	int n = nums.size();
+	if (n < 2)
+		return 0;
+	int min_ele = *min_element(nums.begin(), nums.end());
+	int max_ele = *max_element(nums.begin(), nums.end());
+	int bucket_size = floor((max_ele - min_ele) / (n - 1))-1;
+	int bucket_num = ceil((max_ele - min_ele) / bucket_size) + 1;
+	vector<set<int>> buckets(bucket_num);
+	for (int i = 0; i < n; i++) {
+		int num = floor((nums[i] - min_ele) / bucket_size);
+		buckets[num].insert(num);
+	}
+	int res = INT_MIN;
+	auto pre = buckets[0];
+	for (int i = 1; i < buckets.size();i++) {
+		if (!buckets[i].empty()) {
+			int temp = *buckets[i].begin() - *(--pre.end());
+			if (res < temp)
+				res = temp;
+			pre = buckets[i];
+		}
+	}
+	cout << res;
+
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
