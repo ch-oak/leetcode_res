07ccd0ce28f30fedf9c7acccf3c9d4406a29526d
Chen <choak@qq.com>
2018-12-20 00:45:21 +0800

    1220

---
+++ leetcode_medium/6. ZigZag Conversion/6. ZigZag Conversion.cpp
@@ -0,0 +1,34 @@
+﻿// 6. ZigZag Conversion.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+public:
+	string convert(string s, int numRows) {
+
+	}
+};
+int main()
+{
+	freopen("in.txt", "r", stdin);
+	string s;
+	int numRows;
+	while (cin >> s >> numRows) {
+		cout << s << " " << numRows << endl;
+	}
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
