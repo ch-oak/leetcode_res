6328611674c1978be6ecc1672f6a6d8aaefabccd
Chen <choak@qq.com>
2018-12-26 19:36:55 +0800

    1226

--- leetcode_easy/13. Roman to Integer/13. Roman to Integer.cpp
+++ leetcode_easy/13. Roman to Integer/13. Roman to Integer.cpp
@@ -6,19 +6,20 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <unordered_map>
 
 
 
 using namespace std;
 
 /**
-*比较简单的思路，如果遇到IV,IX之类的就将其置为负数，否则逐个累加。
+*比较简单的思路，如果遇到IV,IX之类的就将I置为负数，否则逐个累加。
 */
-class Solution{
+class Solution1{
 public:
 	int romanToInt(string s ){
 		int res = 0;
-		map<char, int> list{ {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} };
+		unordered_map<char, int> list{ {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} };
 		int len = s.size();
 		for (int i = 0; i < len; i++) {
 			if (s[i] == 'I'&& i + 1 < len && (s[i + 1] == 'V' || s[i + 1] == 'X'))
@@ -34,7 +35,46 @@ public:
 	}
 };
 
+/**
+*比较聪明的办法，从后往前累加，如果前一个数字小于后一个数字，则出现4,9,40，90等情况
+*/
+class Solution2 {
+public:
+	int romanToInt(string s) {
+		int res = 0;
+		unordered_map<char, int> list{ {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} };
+		res += list[s.back()];
+		int len = s.size();
+		for (int i = len-2; i >= 0; i--) {
+			if (list[s[i]] < list[s[i + 1]])
+				res -= list[s[i]];
+			else
+				res += list[s[i]];
+		}
+		return res;
+	}
+};
 
+/**
+*从前往后也可以，注意越界，出现两个字符组成一个数字的时候，前一个字符一定比后面的小。
+*/
+class Solution {
+public:
+	int romanToInt(string s) {
+		int res = 0;
+		unordered_map<char, int> list{ {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} };
+		int len = s.size();
+		for (int i = 0; i < len; i++) {
+			int temp = list[s[i]];
+			if (i < len - 1) {
+				if (list[s[i]] < list[s[i + 1]])
+					temp = -temp;
+			}
+			res += temp;
+		}
+		return res;
+	}
+};
 int main()
 {
 	freopen("in.txt", "r", stdin);
@@ -43,7 +83,6 @@ int main()
 	while (cin >> temp) {
 		cout << sol.romanToInt(temp) << endl;
 	}
-    std::cout << "Hello World!\n"; 
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
