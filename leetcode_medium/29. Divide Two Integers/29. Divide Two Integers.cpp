66bb9af3d1e74212c2e7696258cc3db9b4691d3d
Chen <choak@qq.com>
2019-01-12 14:13:40 +0800

    20190112

---
+++ leetcode_medium/29. Divide Two Integers/29. Divide Two Integers.cpp
@@ -0,0 +1,56 @@
+﻿// 29. Divide Two Integers.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//不用乘、除、位操作计算整数除整数
+
+#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+/**
+*首先不是用减法这么简单
+*/
+
+class Solution {
+public:
+	int divide(int dividend, int divisor) {
+		int res = 0;
+		int flag = 1;
+		long long dividend_long = long long(dividend);
+		long long divisor_long = long long(divisor);
+		if ((dividend_long < 0 &&divisor_long > 0)||(dividend_long >0 && divisor_long < 0)) 
+			flag = -1;
+		dividend_long = -dividend_long;
+		divisor_long = abs(divisor_long);
+		while (dividend_long>=divisor_long) {
+			dividend_long -= divisor_long;
+			res++;
+		}
+		return flag*res;
+	}
+};
+
+int main()
+{
+	freopen("in.txt", "r", stdin);
+	int dividend, divisor;
+	Solution sol;
+	while (cin >> dividend >> divisor) {
+		cout << sol.divide(dividend, divisor) << endl;
+	}
+	
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
