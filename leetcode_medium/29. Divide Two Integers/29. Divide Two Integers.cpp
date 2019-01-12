1f3a4dc89f3f43e29031ed9b52b15cb9204efd99
Chen <choak@qq.com>
2019-01-12 21:28:59 +0800

    20190112

--- leetcode_medium/29. Divide Two Integers/29. Divide Two Integers.cpp
+++ leetcode_medium/29. Divide Two Integers/29. Divide Two Integers.cpp
@@ -1,5 +1,5 @@
 ﻿// 29. Divide Two Integers.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//不用乘、除、位操作计算整数除整数
+//不用乘、除、余数操作计算整数除整数
 
 #include "pch.h"
 #define _CRT_SECURE_NO_WARNINGS
@@ -10,7 +10,7 @@
 using namespace std;
 
 /**
-*首先不是用减法这么简单
+*首先不是用减法这么简单。结果就是减法这么简单，用位运算不断增加除数，同时记录移动的位数加到结果上
 */
 
 class Solution {
@@ -18,15 +18,22 @@ public:
 	int divide(int dividend, int divisor) {
 		int res = 0;
 		int flag = 1;
-		long long dividend_long = long long(dividend);
-		long long divisor_long = long long(divisor);
-		if ((dividend_long < 0 &&divisor_long > 0)||(dividend_long >0 && divisor_long < 0)) 
+		if (dividend == INT_MIN&&divisor==-1)
+			return INT_MAX;
+		if ((dividend< 0 &&divisor > 0)||(dividend >0 && divisor < 0)) 
 			flag = -1;
-		dividend_long = -dividend_long;
-		divisor_long = abs(divisor_long);
-		while (dividend_long>=divisor_long) {
-			dividend_long -= divisor_long;
-			res++;
+		long long dvd = labs(dividend);
+		long long dvs = labs(divisor);
+		if (divisor == 1)
+			return flag * dvd;
+		while (dvd>=dvs) {
+			long long mul = 1,temp = dvs;//这里如果用int的话会报错
+			while (dvd > (temp << 1)) {
+				temp <<= 1;
+				mul <<= 1;
+			}
+			dvd -= temp;
+			res += mul;
 		}
 		return flag*res;
 	}
