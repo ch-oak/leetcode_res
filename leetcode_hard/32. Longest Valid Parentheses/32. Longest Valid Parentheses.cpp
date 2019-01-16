0678b9b4024678b4ccd618af34194306c87847fe
Chen <choak@qq.com>
2019-01-17 00:31:54 +0800

    20190117

--- leetcode_hard/32. Longest Valid Parentheses/32. Longest Valid Parentheses.cpp
+++ leetcode_hard/32. Longest Valid Parentheses/32. Longest Valid Parentheses.cpp
@@ -14,6 +14,8 @@ using namespace std;
 
 /**
 *首先想用栈来考虑到前面做的有效括号判断，想用栈来判断，但是不太合适。这里求的最长的连续的有效括号
+*参考：https://leetcode.com/problems/longest-valid-parentheses/discuss/14126/My-O(n)-solution-using-a-stack
+*用栈记录所有异常点，巧妙之处在于有效括号一定是连续的，对于异常括号的存储，存储的是下标
 */
 
 class Solution {
@@ -22,12 +24,32 @@ public:
 		if (s.empty())
 			return 0;
 		int len = s.size();
-		for (int i = 0; i < len; i++) {
-
-		}
 		stack<int> index;
-
-		return 0;
+		for (int i = 0; i < len; i++) {
+			if (s[i] == '(') index.push(i);
+			else {
+				if (index.empty())
+					index.push(i);
+				else if (s[index.top()] == '(')
+					index.pop();
+				else
+					index.push(i);
+			}
+	
+		}
+		if (index.empty())
+			return len;
+		int max = 0;
+		int next = len;
+		int cur;
+		while (!index.empty()) {
+			cur = index.top();
+			max = max > next-cur - 1 ? max : next - cur - 1;
+			next = cur;
+			index.pop();
+		}
+		max = max > next  ? max : next;
+		return max;
 	}
 };
 int main()
