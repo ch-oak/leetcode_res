2ed8e62def7cea87fd876abeda960e9462a596fc
Chen <choak@qq.com>
2019-02-15 23:59:00 +0800

    1

--- leetcode_medium/Permutations II/Permutations II.cpp
+++ leetcode_medium/Permutations II/Permutations II.cpp
@@ -2,8 +2,11 @@
 //
 
 #include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 using namespace std;
 class Solution {
@@ -15,6 +18,15 @@ public:
 
 int main()
 {
+	freopen("in.txt", "r", stdin);
+	string line;
+	while (cin >> line) {
+		stringstream ss(line);
+		string temp;
+		vector<int> nums;
+		while (getline(ss, temp, ","))
+			nums.push_back(stoi(temp));
+	}
     std::cout << "Hello World!\n"; 
 }
 
