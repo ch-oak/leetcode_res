3b840f482a39c923e5da30c7181bfda18d6ac349
Chen <choak@qq.com>
2019-02-20 13:41:11 +0800

    leetcode 58

---
+++ leetcode_easy/58. Length of Last Word/58. Length of Last Word.cpp
@@ -0,0 +1,48 @@
+﻿// 58. Length of Last Word.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+	int lengthOfLastWord(string s) {
+		int pre, next = -1;
+		for (int i = s.size() - 1; i >= 0; i--) {
+			if (s[i] != ' ') {
+				pre = i;
+				break;
+			}
+		}
+		if (pre == -1)
+			return 0;
+		for (int j = pre; j >= 0; j--) {
+			if (s[j] == ' ') {
+				next = j;
+				break;
+			}
+		}
+		return pre - next;
+	}
+};
+
+int main()
+{
+
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
