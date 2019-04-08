622501bb13268003b4b3e35789cf27f9f7ab1697
Chen <choak@qq.com>
2019-04-08 13:19:37 +0800

    64. Minimum Path Sum

---
+++ leetcode_medium/64. Minimum Path Sum/64. Minimum Path Sum.cpp
@@ -0,0 +1,52 @@
+﻿// 64. Minimum Path Sum.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+*动态规划：dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+*/
+class Solution {
+public:
+	int minPathSum(vector<vector<int>>& grid) {
+		if (grid.empty())
+			return 0;
+		int row = grid.size();
+		int col = grid[0].size();
+		vector<vector<int>> dp(row, vector<int>(col, 0));
+		dp[0][0] = grid[0][0];
+		for (int j = 1; j < col; j++)
+			dp[0][j] = dp[0][j - 1] + grid[0][j];
+		for (int i = 1; i < row; i++)
+			dp[i][0] = dp[i - 1][0] + grid[i][0];
+		for (int i = 1; i < row; i++) {
+			for (int j = 1; j < col; j++) {
+				dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+			}
+		}
+		return dp[row - 1][col - 1];
+	}
+};
+
+int main()
+{
+	vector<vector<int>> grid{ {1,3,1},{1,5,1},{4,2,1} };
+	cout << Solution().minPathSum(grid);
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
