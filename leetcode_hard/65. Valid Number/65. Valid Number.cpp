eda44715e6d89b4323bfe6d88786cd39961b418b
Chen <choak@qq.com>
2019-04-09 13:12:02 +0800

    20190409

---
+++ leetcode_hard/65. Valid Number/65. Valid Number.cpp
@@ -0,0 +1,43 @@
+﻿// 65. Valid Number.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+
+
+/**
+有限状态机或者正则表达式：https://blog.csdn.net/zhaoqinmuxue/article/details/88044125
+*/
+
+class Solution {
+public:
+	bool isNumber(string s) {
+		s.erase(0, s.find_first_not_of(" "));
+		s.erase(s.find_last_not_of(" ") + 1);
+		if (s.empty())  return false;
+		string p = "[+-]?(\\d+\\.?|\\.\\d+)\\d*(e[+-]?\\d+)?";
+		return regex_match(s, regex(p));
+	}
+};
+
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
