a1a4778fc2f413137041c929e48f23c92c5ea8f2
Chen <choak@qq.com>
2019-04-26 14:01:59 +0800

    104.Construc Binary Tree From Preorder and Inorder

---
+++ leetcode_easy/104. Maximum Depth of Binary Tree/104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,77 @@
+﻿// 104. Maximum Depth of Binary Tree.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+
+//Definition for a binary tree node.
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//参考102题二叉树的层序遍历，每增加一层，就递增结果
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+		if (!root)
+			return 0;
+		int level = 1;
+		queue<TreeNode*> cur{ {root} };
+		while (!cur.empty()) {
+			for (int i = cur.size(); i > 0; i--) {
+				TreeNode* tp = cur.front();
+				cur.pop();
+				if (tp->left) cur.push(tp->left);
+				if (tp->right) cur.push(tp->right);
+			}
+			if (!cur.empty())
+				level++;
+		}
+		return level;
+	}
+};
+
+
+//简单的深度优先搜索：https://www.cnblogs.com/bakari/p/4126693.html
+class Solution {
+public:
+	int maxDepth(TreeNode* root) {
+		if (!root)
+			return 0;
+		int l = maxDepth(root->left);
+		int r = maxDepth(root->right);
+		return l > r ? l + 1 : r + 1;
+	}
+};
+
+
+int main()
+{
+	TreeNode* root = new TreeNode(1);
+	root->left = new TreeNode(2);
+	root->right = new TreeNode(3);
+	root->left->left = new TreeNode(4);
+	root->right->right = new TreeNode(5);
+	cout << Solution().maxDepth(root);
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
---
+++ leetcode_medium/105. Construct Binary Tree from Preorder and Inorder Traversal/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -0,0 +1,51 @@
+﻿// 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//根据二叉树前序遍历和中序遍历的结果，构建二叉树
+//Array Tree Depth-first Search
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+//Definition for a binary tree node.
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+//递归：https://www.cnblogs.com/springfor/p/3884034.html
+class Solution {
+public:
+	TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+		return buildTree(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1);
+	}
+private:
+	TreeNode* buildTree(vector<int>& preorder, int preStart, int preEnd, vector<int>& inorder, int inStart, int inEnd) {
+		if (preStart > preEnd || inStart > inEnd)//到尽头了，只用一个条件应该也可以
+			return NULL;
+		int rootVal = preorder[preStart];
+		int rootIndex = find(inorder.begin(),inorder.end(),rootVal) - inorder.begin();
+		int lenLeft = rootIndex - inStart;
+		TreeNode* root = new TreeNode(rootVal);
+		root->left = buildTree(preorder, preStart + 1, preStart + lenLeft, inorder, inStart, rootIndex - 1);
+		root->right = buildTree(preorder, preStart + lenLeft + 1,preEnd, inorder, rootIndex + 1, inEnd);
+	}
+};
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
