b69c683a1d13d486727777110fa8adaec839a604
Chen <choak@qq.com>
2019-05-09 19:53:50 +0800

    287.找出重复数字

---
+++ leetcode_medium/287. Find the Duplicate Number/287. Find the Duplicate Number.cpp
@@ -0,0 +1,57 @@
+﻿// 287. Find the Duplicate Number.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//https://leetcode.com/problems/find-the-duplicate-number/
+//找出重复数字(数字在1-n之间)
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+	int findDuplicate(vector<int>& nums) {
+		for (int i = 0; i < nums.size(); i++) {
+			for (int j = i + 1; j < nums.size(); j++) {
+				if (nums[i] == nums[j])
+					return nums[i];
+			}
+		}
+		return 1;
+	}
+};
+
+//用查找循环链表环开始的节点的方法来做
+//参考：https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
+class Solution {
+public:
+	int findDuplicate(vector<int>& nums) {
+		int slow = nums[0];
+		int fast = nums[nums[0]];
+		while (slow != fast) {
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		}
+		fast = 0;
+		while (slow != fast) {
+			slow = nums[slow];
+			fast = nums[fast];
+		}
+		return slow;
+	}
+};
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
