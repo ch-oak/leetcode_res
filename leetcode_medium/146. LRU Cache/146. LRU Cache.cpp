516fa58a2124a8231dacd1f5ce039ff5f57e84d7
Chen <choak@qq.com>
2019-06-28 16:35:39 +0800

    0628#1635

---
+++ leetcode_medium/146. LRU Cache/146. LRU Cache.cpp
@@ -0,0 +1,91 @@
+﻿// 146. LRU Cache.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+//https://leetcode.com/problems/lru-cache/
+#include "pch.h"
+#include <iostream>
+#include <list>
+#include <unordered_map>
+
+using namespace std;
+
+
+/*
+主要就是定义出这个数据结构：unordered_map<int, pair<int, list<int>::iterator>> cache;分别表示key，value，最近使用的key的链表
+也可以分成两个map，一个表示key,value 一个表示key,iterator
+查找：
+直接在map里查找key,O(1)，就找到了对应的value和key所在的链表的迭代器，删除该迭代器，将value插入到表头，更新value对应的迭代器为表头。
+插入：
+	存在key,将value更新，同时将与value相连的key放到表头，更新map
+	不存在key
+		满了，删除链表尾巴元素，及map中对应的key，中心插入
+		没满，将key插入表头，与value相连
+*/
+
+class LRUCache {
+public:
+	LRUCache(int capacity) {
+		sz = capacity;
+	}
+
+	//设置或者查询key都算一次操作
+	int get(int key) {
+		auto it = cache.find(key);
+		if (it != cache.end()) {
+			update(cache, it);
+			return it->second.first;
+		}
+		return -1;
+	}
+
+	void put(int key, int value) {
+		auto it = cache.find(key);
+		if (it != cache.end()) {
+			it->second.first = value;
+			update(cache, it);
+			return;
+		}
+		if (cache.size() == sz) {
+			int oldKey = used.back();//在链表里存key的原因就在于此，方便map的删除
+			cache.erase(oldKey);
+			used.pop_back();
+			used.push_front(key);
+			cache[key] = { value,used.begin() };
+		}
+		else {
+			used.push_front(key);
+			cache[key] = { value,used.begin() };
+		}
+	}
+private:
+	int sz;
+	unordered_map<int, pair<int, list<int>::iterator>> cache;
+	list<int> used;//最近使用的key的链表
+
+	void update(unordered_map<int, pair<int, list<int>::iterator>> &cache, unordered_map<int, pair<int, list<int>::iterator>>::iterator it) {
+		int key = it->first;
+		used.erase(it->second.second);
+		used.push_front(key);
+		it->second.second = used.begin();
+
+		//cache.erase(it);
+		//cache[key] = { val,used.begin() };
+	}
+};
+
+
+int main()
+{
+	LRUCache cache = LRUCache(2 /* capacity */);
+
+	cache.put(1, 1);
+	cache.put(2, 2);
+	cout << cache.get(1) << endl;       // returns 1
+	cache.put(3, 3);    // evicts key 2
+	cout << cache.get(2) << endl;       // returns -1 (not found)
+	cache.put(4, 4);    // evicts key 1
+	cout << cache.get(1) << endl;       // returns -1 (not found)
+	cout << cache.get(3) << endl;       // returns 3
+	cout << cache.get(4) << endl;       // returns 4
+    std::cout << "Hello World!\n"; 
+}
+
---
+++ mi_oj/190606_game/190606_game.cpp
@@ -0,0 +1,55 @@
+﻿// 190606_game.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	//freopen("in.txt", "r", stdin);
+	//int t;
+	//cin >>t;
+	//while (t--) {
+	//	int n, k;
+	//	cin >> n >> k;
+	//	double res = k * 0.5;
+	//	long long  i_res = 100 * res;
+	//	res = i_res / 100.0;
+	//	
+	//	if (t != 0) {
+	//		printf("%.2f\n", res);
+	//	}
+	//	else {
+	//		printf("%.2f", res);
+	//	}
+
+	//}
+	cout << 16 % 1000000007;
+	int n;
+	cin >> n;
+	int n_ = n;
+	vector<pair<int, int>> points;
+	while (n--) {
+		int x, y;
+		cin >> x >> y;
+		points.push_back({ x,y });
+	}
+
+	return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
